bb090c2412117fe7887db3d04b804ce2b856c5e5
Traveler3114 <matej.krizic16@gmail.com>
2025-09-03 06:48:48 +0200

    Update ItemBase.cpp

--- Source/EchoesOfTime/InventorySystem/ItemBase.cpp
+++ Source/EchoesOfTime/InventorySystem/ItemBase.cpp
@@ -18,7 +18,7 @@ void UItemBase::OnDropped_Implementation(AActor* Instigator, FGuid ItemInstanceI
     UWorld* World = Instigator->GetWorld();
     if (!World) return;
 
-    FRotator SpawnRotation = FRotator::ZeroRotator;
+    FRotator SpawnRotation = FRotator(90.0f,0.0f,0.0f);
     FTransform SpawnTransform = FTransform(SpawnRotation, DropLocation);
 
     AItemPickup* Pickup = World->SpawnActorDeferred<AItemPickup>(AItemPickup::StaticClass(), SpawnTransform);
